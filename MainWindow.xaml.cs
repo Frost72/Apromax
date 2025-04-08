802582333833231ed93c14594940b0a93f6d95cd
Александр <133597556+Frost72@users.noreply.github.com>
2025-04-08 13:55:56 +0300

    form

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,59 @@ namespace Apromax
         {
             InitializeComponent();
         }
+
+
+        private void LineMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Line line = new Line();
+            line.Show();
+
+        }
+        private void QuadraticMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Quadratic quadratic = new Quadratic();
+            quadratic.Show();
+        }
+        private void CubicMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Cubic cubic = new Cubic();
+            cubic.Show();
+        }
+       private void LagranjMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Lagranj lagranj = new Lagranj();
+            lagranj.Show();
+        }
+
+        private void NutonMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Nuton nuton = new Nuton();  
+            nuton.Show();
+        }
+        private void MnkMethod_Click(object sender, RoutedEventArgs e)
+        {
+            MNK mNK = new MNK();
+            mNK.Show();
+        }
+
+        private void TeilorMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Teilor tilor = new Teilor();
+            tilor.Show();
+        }
+
+        private void ChebshevMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Chebshev chebshev = new Chebshev();
+            chebshev.Show();
+        }
+
+        private void FurieMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Furie furie = new Furie();
+            furie.Show();
+        }
+
+     
     }
 }
